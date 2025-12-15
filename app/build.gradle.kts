240d5c07d1f00feb16f3ceaccb2872f2d631686f
Marcel Reiter <marcel.reiter@pixida.de>
2025-12-15 17:39:50 +0100

    Add and use Lottie compose as one very prominent dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.lottie)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/anfema/testort/MainActivity.kt
+++ app/src/main/java/com/anfema/testort/MainActivity.kt
@@ -7,11 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.anfema.testort.ui.theme.TestOrtTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,9 +43,24 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding)
                     ) {
 
+                        Lottie(
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .size(100.dp)
+                        )
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+private fun Lottie(modifier: Modifier = Modifier) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie))
+    LottieAnimation(
+        composition = composition,
+        iterations = 5,
+        modifier = modifier,
+    )
 }
