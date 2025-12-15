3710eb151842cd0aa8cdd54a93cd0dcd6845e675
Marcel Reiter <marcel.reiter@pixida.de>
2025-12-15 17:38:39 +0100

    Remove demo code

--- app/src/main/java/com/anfema/testort/MainActivity.kt
+++ app/src/main/java/com/anfema/testort/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.anfema.testort.ui.theme.TestOrtTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TestOrtTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestOrtTheme {
-        Greeting("Android")
     }
 }
