c6bbfeb1c6f16ab18138ca864973f9b350ae244c
Marcel Reiter <marcel.reiter@pixida.de>
2025-12-15 17:39:10 +0100

    Add minimal test screen

--- app/src/main/java/com/anfema/testort/MainActivity.kt
+++ app/src/main/java/com/anfema/testort/MainActivity.kt
@@ -7,26 +7,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.anfema.testort.ui.theme.TestOrtTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             TestOrtTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    Box(
-                        Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    ) {
-
-                    }
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .padding(32.dp)
+                        .fillMaxSize(),
+                ) {
+                    Text(
+                        text = "ORT Test",
+                        style = MaterialTheme.typography.displayMedium,
+                        textAlign = TextAlign.Center
+                    )
                 }
             }
         }
