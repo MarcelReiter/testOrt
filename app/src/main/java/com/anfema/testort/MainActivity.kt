20b8380b518b4deccbd9ed0c9533bdc594b48b2c
Marcel Reiter <marcel.reiter@pixida.de>
2025-12-15 17:39:10 +0100

    Add TopAppBar

--- app/src/main/java/com/anfema/testort/MainActivity.kt
+++ app/src/main/java/com/anfema/testort/MainActivity.kt
@@ -7,17 +7,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import com.anfema.testort.ui.theme.TestOrtTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TestOrtTheme {
                 Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Lottie Test") }
+                        )
+                    },
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
                     Box(
